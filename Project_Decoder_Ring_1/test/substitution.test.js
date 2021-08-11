fff845404c2120c7fca0d771df67ec3457b5dace
Hasan Jessa <84889841+hasanjessa@users.noreply.github.com>
2021-08-12 03:53:15 +0500

    Update substitution.test.js

--- Project_Decoder_Ring_1/test/substitution.test.js
+++ Project_Decoder_Ring_1/test/substitution.test.js
@@ -1,39 +1,39 @@
 // Write your tests here!
-const subsitution = require("../src/substitution")
+const {substitution} = require("../src/substitution")
 const expect = require("chai").expect;
 
-describe("subsitution()", () => {
+describe("substitution()", () => {
 
 	const alpha = "xoyqmcgrukswaflnthdjpzibev";
     
 	it("should encode correctly", () => {
-		expect(subsitution("thinkful", alpha)).to.equal("jrufscpw");
+		expect(substitution("thinkful", alpha)).to.equal("jrufscpw");
 	});
 
 	it("should decode correctly", () => {
-		expect(subsitution("jrufscpw", alpha, false)).to.equal("thinkful");
+		expect(substitution("jrufscpw", alpha, false)).to.equal("thinkful");
 	});
 	
 	it("should maintain spaces throughout", () => {
-		expect(subsitution("you are an excellent spy", alpha)).to
+		expect(substitution("you are an excellent spy", alpha)).to
 			.equal("elp xhm xf mbymwwmfj dne");
 	});
 
 	it("should ignore capital letters", () => {
-		expect(subsitution("ThInKfUl", alpha)).to.equal("jrufscpw");
+		expect(substitution("ThInKfUl", alpha)).to.equal("jrufscpw");
 	});
 
 	it("should be able to use symbols within the alphabet", () => {
-		expect(subsitution("message", "$wae&zrdxtfcygvuhbijnokmpl")).to
+		expect(substitution("message", "$wae&zrdxtfcygvuhbijnokmpl")).to
 			.equal("y&ii$r&");
 	});
 
 	it("should return false if alphabet does not have 26 characters", () => {
-		expect(subsitution("thinkful", "short")).to.equal(false);
+		expect(substitution("thinkful", "short")).to.equal(false);
 	});
 
 	it("should return false if alphabet contains non-unique characters", () => {
-		expect(subsitution("thinkful", "abcabcabcabcabcabcabcabcyz")).to
+		expect(substitution("thinkful", "abcabcabcabcabcabcabcabcyz")).to
 			.equal(false);
 	});
 });
